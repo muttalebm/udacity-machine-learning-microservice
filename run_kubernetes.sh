4db357fe3e04988a93c8a59370fb52b76ab8b458
Abdul Muttaleb Maruf <ammaruf7@gmail.com>
2020-04-03 01:38:05 +0600

    review fix

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,11 +9,14 @@ dockerpath=muttalebm/machine-learning-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run development --image=$dockerpath --port=80
+kubectl create deployment mlcluster --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment development --type=LoadBalancer --port=8000 --target-port=80
+kubectl proxy
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
+
